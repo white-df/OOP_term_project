f82906d60be32471a619dc059456776f788eaace
Jongoo Kim <coolkjs7@kakao.com>
2021-10-27 19:57:34 +0900

    First Commit

---
+++ OOP_term_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP_term_project
+//
+//  Created by 김종수 on 2021/10/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
