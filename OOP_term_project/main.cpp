aa1bc06114f4194e40f4088bd8ef236f94ed4910
Jongoo Kim <coolkjs7@kakao.com>
2021-10-27 20:29:22 +0900

    뼈대

--- OOP_term_project/main.cpp
+++ OOP_term_project/main.cpp
@@ -1,14 +1,63 @@
-//
-//  main.cpp
-//  OOP_term_project
-//
-//  Created by 김종수 on 2021/10/27.
-//
+/*
+***   ********************************************************
+***   *                                                      *
+***   *   Object-Oriented Programming Course Term Project    *
+***   *                                                      *
+***   *   201511047 Kim,Jong-Soo    201611030 Kim,San-Ha     *
+***   *   201911012 Kong,Young-Jae  201911088 An,Sang-Jun    *
+***   *                                                      *
+***   ********************************************************
+ */
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+/*
+***  Class Building
+ */
+
+/*-- Account Class --*/
+class Account {
+private:
+    string accountNumber;
+    string accountPw;
+    string cardNum;
+    string userName;
+public:
+    Account() {}
+    ~Account() {}
+    int check_pw(string inPw) {
+        if (accountPw == inPw) return 0;
+        else return 1;
+    }
+    
+};
+
+/*-- Bank Class --*/
+class Bank {
+private:
+    string bankName;
+    Account* accounts;
+public:
+    
+};
+
+/*-- Bank Class --*/
+class ATM {
+private:
+    string serialNum;
+    Bank primaryBank;
+    int atmType;    // 0: Single-Bank ATM,    1: Multi-Bank ATM
+    int lingualType;    // 0: unilingual(only English), 1: bilingual(English or Korean)
+    int language;   // 0: Korean,    1: English
+    unsigned long long inCash;  // ATM에 들어있는 현금
+public:
+    
+};
+
+/*----- Main Function -----*/
+int main() {
+    
     return 0;
 }
