07ac8dca5d5db64ae5613cc7b948fa0b32794f49
Jongoo Kim <coolkjs7@kakao.com>
2021-11-29 22:52:21 +0900

    Transaction 추가

--- OOP_term_project/main.cpp
+++ OOP_term_project/main.cpp
@@ -463,6 +463,7 @@ void readAtmData(ifstream& fin) {
             ATM* newAtm;
             if (splitted[1].compare("Single")) {
                 newAtm = new SingleBankATM(splitted[0]);
+                
             } else if (splitted[1].compare("Multi")) {
                 newAtm = new MultiBankATM(splitted[0]);
             }
